0a90d43b4a800489e258ec141c95e9d13cc7fc27
wangce <wangce@ctvit.com.cn>
2023-04-14 18:02:40 +0800

    delete 更新

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,24 @@
 <template>
   <div class="container-xxl pb-4">
-    <m-header />
+    <m-header ref="header" />
   </div>
   <router-view></router-view>
 </template>
 
 <script lang="ts">
-import { computed, defineComponent } from 'vue'
+import { computed, defineComponent, ref } from 'vue'
 import mHeader from '@/layout/mHeader.vue'
 
 export default defineComponent({
   name: 'App',
   components: { mHeader },
   setup() {
+    const header = ref()
+    console.log(header, 'headerheader')
     const ifAside = computed(() => {
       return false
     })
-    return { ifAside }
+    return { ifAside, header }
   }
 })
 </script>
